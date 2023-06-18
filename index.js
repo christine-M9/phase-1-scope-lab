20a69927c315f5d365f817e83794b593c815cf7e
Christine <christinekhekama99@gmail.com>
2023-06-18 15:59:18 +0300

    complete assignment

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ var customerName = "bob";
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase();
   }
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'Tee';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Hem'; 
+  }
+  
